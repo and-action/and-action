878d85a3bf177e4622b17eac70f23f517107273a
Pascal Chorus <p.chorus@me.com>
2020-03-21 13:53:51 +0100

    refactor(config): add config service to main process

---
+++ dashboard-config.service.ts
@@ -0,0 +1,49 @@
+import { ipcMain } from 'electron';
+import { IpcChannel } from './ipc-channel';
+import * as fs from 'fs';
+import { ActionsDashboardConfig } from './src/app/core/actions-dashboard-config';
+
+const DASHBOARD_CONFIG_FILENAME = 'dashboard-config.json';
+
+export class DashboardConfigService {
+  constructor() {
+    this.initListeners();
+  }
+
+  private initListeners() {
+    ipcMain.on(
+      IpcChannel.SAVE_ACTIONS_DASHBOARD_CONFIG,
+      (event: any, actionsDashboardConfig: ActionsDashboardConfig) =>
+        fs.writeFile(
+          DASHBOARD_CONFIG_FILENAME,
+          JSON.stringify(actionsDashboardConfig, undefined, 2),
+          () =>
+            event.sender.send(
+              IpcChannel.SAVE_ACTIONS_DASHBOARD_CONFIG_RESPONSE,
+              true
+            )
+        )
+    );
+
+    // TODO: refactor
+    ipcMain.on(IpcChannel.LOAD_ACTIONS_DASHBOARD_CONFIG, (event: any) => {
+      if (fs.existsSync(DASHBOARD_CONFIG_FILENAME)) {
+        fs.readFile(
+          DASHBOARD_CONFIG_FILENAME,
+          { encoding: 'utf8' },
+          (err, data) =>
+            event.sender.send(
+              IpcChannel.LOAD_ACTIONS_DASHBOARD_CONFIG_RESPONSE,
+              JSON.parse(data)
+            )
+        );
+      } else {
+        event.sender.send(
+          IpcChannel.LOAD_ACTIONS_DASHBOARD_CONFIG_RESPONSE,
+          new ActionsDashboardConfig([])
+        );
+        return new ActionsDashboardConfig([]);
+      }
+    });
+  }
+}
