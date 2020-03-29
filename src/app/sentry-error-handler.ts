68fadb864f307d603b0a8229f357c6ee8e557884
Pascal Chorus <p.chorus@me.com>
2020-03-29 20:37:01 +0200

    feat(sentry): add sentry to angular app

---
+++ src/app/sentry-error-handler.ts
@@ -0,0 +1,19 @@
+import * as Sentry from '@sentry/browser';
+import { ErrorHandler, Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+
+if (environment.sentryDsn) {
+  Sentry.init({
+    dsn: environment.sentryDsn,
+    environment: environment.name
+  });
+}
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {}
+
+  handleError(error) {
+    Sentry.captureException(error.originalError || error);
+  }
+}
