14da71d52a289254f1a3ce698b5491de84bd7d9e
Pascal Chorus <p.chorus@me.com>
2020-04-01 21:00:50 +0200

    feat(analytics): add google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LoginService } from './core/login.service';
 import { AppRouting } from './app-routing';
+import { environment } from '../environments/environment';
+import { EnvironmentName } from '../environments/environment-name';
 
 @Component({
   selector: 'ana-root',
@@ -23,5 +25,30 @@ export class AppComponent implements OnInit {
           .subscribe(() => this.router.navigate(['/', AppRouting.DASHBOARD]));
       }
     });
+
+    if (environment.name === EnvironmentName.PRODUCTION) {
+      this.initGoogleTagManager();
+    }
+  }
+
+  private initGoogleTagManager() {
+    const trackingId = 'UA-162485821-1';
+    const gTagScript = document.createElement('script');
+    gTagScript.src = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`;
+
+    document.getElementsByTagName('head')[0].appendChild(gTagScript);
+
+    const gTagScript2 = document.createElement('script');
+    gTagScript2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+  
+      gtag('config', '${trackingId}');
+    `;
+
+    document.getElementsByTagName('head')[0].appendChild(gTagScript2);
   }
 }
