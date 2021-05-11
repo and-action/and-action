c95120979ff097b3a538b227f60039e02ce3334f
Pascal Chorus <p.chorus@me.com>
2021-05-11 21:04:14 +0200

    test(coverage): replace istanbul-coverage with karma-coverage

--- karma.conf.js
+++ karma.conf.js
@@ -12,13 +12,13 @@ module.exports = function(config) {
       require('karma-chrome-launcher'),
       require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, './coverage'),
       reports: ['html', 'lcovonly', 'text-summary']
     },
