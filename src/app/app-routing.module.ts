b5784456f93d0ad2b837451cb0cabde58d831d8a
Pascal Chorus <p.chorus@me.com>
2021-05-20 21:35:35 +0200

    fix(commits): repository-config must be set for commits view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AppRouting } from './app-routing';
 import { ActionsDashboardComponent } from './actions-dashboard/actions-dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { ActionsDashboardConfigComponent } from './actions-dashboard-config/actions-dashboard-config.component';
-import { ActionsDashboardGuard } from './actions-dashboard/actions-dashboard.guard';
+import { RepositoryConfigGuard } from './repository-config.guard';
 import { CommitsDashboardComponent } from './commits-dashboard/commits-dashboard.component';
 import { LoginGuard } from './login.guard';
 
@@ -12,7 +12,7 @@ const routes: Routes = [
   {
     path: AppRouting.DASHBOARD,
     component: ActionsDashboardComponent,
-    canActivate: [LoginGuard, ActionsDashboardGuard],
+    canActivate: [LoginGuard, RepositoryConfigGuard],
   },
   {
     path: AppRouting.DASHBOARD_CONFIG,
@@ -22,7 +22,7 @@ const routes: Routes = [
   {
     path: AppRouting.COMMITS,
     component: CommitsDashboardComponent,
-    canActivate: [LoginGuard],
+    canActivate: [LoginGuard, RepositoryConfigGuard],
   },
   {
     path: AppRouting.LOGIN,
