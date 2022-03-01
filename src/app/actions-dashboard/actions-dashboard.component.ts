6e8c1930f7b448008bf7af50c75f9f4b75fc146e
Pascal Chorus <p.chorus@me.com>
2022-03-01 20:51:32 +0100

    feat(commits): apply repository filter for actions dashboard

--- src/app/actions-dashboard/actions-dashboard.component.ts
+++ src/app/actions-dashboard/actions-dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AndActionDataService } from '../core/and-action-data.service';
 import { GithubDataService } from '../core/github-data.service';
-import { flatMap, tap } from 'rxjs/operators';
+import { flatMap, map, mergeMap, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Organization } from '../core/organization';
 import { GithubViewer } from '../core/github-viewer';
 import { StatusIconService } from '../status-icon.service';
+import { RepositoryFilterService } from '../repository-filter.service';
 
 @Component({
   selector: 'ana-actions-dashboard',
@@ -18,7 +19,8 @@ export class ActionsDashboardComponent implements OnInit {
   constructor(
     private githubDataService: GithubDataService,
     private andActionDataService: AndActionDataService,
-    private statusIconService: StatusIconService
+    private statusIconService: StatusIconService,
+    private repositoryFilterService: RepositoryFilterService
   ) {}
 
   ngOnInit(): void {
@@ -33,6 +35,24 @@ export class ActionsDashboardComponent implements OnInit {
                 this.statusIconService.updateStatusIcon(viewerAndOrganizations)
               )
             )
+        ),
+        mergeMap((viewerAndOrganizations) =>
+          this.repositoryFilterService.filterValue$.pipe(
+            map((filterValue) =>
+              viewerAndOrganizations
+                .map((viewerAndOrganization) => ({
+                  ...viewerAndOrganization,
+                  repositories: viewerAndOrganization.repositories.filter(
+                    (repository) =>
+                      !filterValue || repository.name.includes(filterValue)
+                  ),
+                }))
+                .filter(
+                  (viewerAndOrganization) =>
+                    viewerAndOrganization.repositories.length > 0
+                )
+            )
+          )
         )
       );
   }
