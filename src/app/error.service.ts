60320dfe95a05e1a82c690e33a4cd18aeb66078a
Pascal Chorus <p.chorus@me.com>
2023-07-20 20:46:58 +0200

    feat(app): suppress unknown error when browser is offline

--- src/app/error.service.ts
+++ src/app/error.service.ts
@@ -3,6 +3,8 @@ import { ApolloError } from '@apollo/client/core';
 
 import { SnackBarService } from './snack-bar/snack-bar.service';
 import { captureException } from '../utils/log-utils';
+import { HttpErrorResponse } from '@angular/common/http';
+import { HttpStatus } from './http-status';
 
 @Injectable({
   providedIn: 'root',
@@ -11,13 +13,32 @@ export class ErrorService {
   private snackBarService = inject(SnackBarService);
 
   handleError(error: any) {
-    captureException(error.originalError ?? error);
     console.error(error);
 
+    if (this.suppressMessage(error)) {
+      return;
+    }
+
+    captureException(error.originalError ?? error);
     this.snackBarService.error(
       error instanceof ApolloError
         ? error.message
         : 'Unknown error. Please try again.',
+    );
+  }
+
+  private suppressMessage(error: unknown) {
+    const isUnknownError = (checkError: unknown) =>
+      checkError instanceof HttpErrorResponse &&
+      checkError.status === HttpStatus._0_UNKNOWN_ERROR;
+
+    const isApolloUnknownError = (checkError: unknown) =>
+      checkError instanceof ApolloError &&
+      isUnknownError(checkError.networkError);
+
+    return (
+      (isUnknownError(error) || isApolloUnknownError(error)) &&
+      !window.navigator.onLine
     );
   }
 }
