60f40c56f9aeb571a9159e51d15bea57cdaa1ba5
Pascal Chorus <pascal.chorus@moneymeets.com>
2025-07-09 09:33:46 +0200

    refactor(app): refactor to signal inputs and make link optional

--- src/app/status-with-text/status-with-text.component.ts
+++ src/app/status-with-text/status-with-text.component.ts
@@ -1,16 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { StatusWithTextStatus } from '../core/status-with-text';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'ana-status-with-text',
-  imports: [],
+  imports: [NgTemplateOutlet],
   templateUrl: './status-with-text.component.html',
   styleUrl: './status-with-text.component.scss',
 })
 export class StatusWithTextComponent {
-  @Input({ required: true }) status?: StatusWithTextStatus;
-  @Input({ required: true }) text?: string;
-  @Input({ required: true }) link?: string;
+  status = input.required<StatusWithTextStatus>();
+  text = input.required<string>();
+  link = input<string>();
 
   protected readonly statusWithTextStatus = StatusWithTextStatus;
 }
